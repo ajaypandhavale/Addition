93a53d20d2d674d4f8925e73884fc67cc03924f5
ajaypandhavale <aj40135821@wipro.com>
2025-01-23 16:20:09 +0530

    Add additionn of two numbers

---
+++ src/Addition.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("enter two numbers to perform addition ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		int sum=a+b;
+		System.out.println(" addition of given numbers are "+sum);
+		
+
+	}
+
+}
